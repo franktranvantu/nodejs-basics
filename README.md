f3577e78c7841957ed3224f982bd00888b4bb10e
Frank Tran <franktranvantu@gmail.com>
2021-02-28 17:08:19 +0700

    Update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Handling Parsing Errors with try and catch
+# Handling Status Code Errors
 
-We've dealt with connection errors but there are other things that could go wrong. Let's see if we can catch the other errors that could happen.
+In this section we'll handle HTTP errors.
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const http = require('http');
 
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript.`;
@@ -11,19 +12,25 @@ function printError(error) {
 
 function getProfile(username) {
   try {
-    const request = https.get(`teamtreehouse.com/${username}.json`, res => {
-      let body = '';
-      res.on('data', data => {
-        body += data.toString();
-      });
-      res.on('end', () => {
-        try {
-          const profile = JSON.parse(body);
-          printMessage(username, profile.badges.length, profile.points.JavaScript);
-        } catch (error) {
-          printError(error);
-        }
-      })
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, res => {
+      if (res.statusCode === 200) {
+        let body = '';
+        res.on('data', data => {
+          body += data.toString();
+        });
+        res.on('end', () => {
+          try {
+            const profile = JSON.parse(body);
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+          } catch (error) {
+            printError(error);
+          }
+        })
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[res.statusCode]}).`;
+        const error = new Error(message);
+        printError(error);
+      }
     });
     request.on('error', printError);
   } catch (error) {
